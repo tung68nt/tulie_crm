f3965e67e36b2b14206dc15e32ee2359f21a1a95
Tulie Lab <tung68nt@users.noreply.github.com>
2026-02-03 13:44:03 +0700

    style(ui): synchronize DataTable side margins with detail views

--- components/shared/data-table.tsx
+++ components/shared/data-table.tsx
@@ -123,7 +123,7 @@ export function DataTable<TData, TValue>({
                             <TableRow key={headerGroup.id}>
                                 {headerGroup.headers.map((header) => {
                                     return (
-                                        <TableHead key={header.id}>
+                                        <TableHead key={header.id} className="first:pl-6 last:pr-6">
                                             {header.isPlaceholder
                                                 ? null
                                                 : flexRender(
@@ -144,7 +144,7 @@ export function DataTable<TData, TValue>({
                                     data-state={row.getIsSelected() && 'selected'}
                                 >
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
+                                        <TableCell key={cell.id} className="first:pl-6 last:pr-6">
                                             {flexRender(
                                                 cell.column.columnDef.cell,
                                                 cell.getContext()
